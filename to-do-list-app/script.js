bf24c94bbff4a464718a3ad96c7acb7cf64723f2
Navaneesh-karepakula <iamnavaneesh@gmail.com>
2025-06-30 22:19:14 +0530

    local storage

--- to-do-list-app/script.js
+++ to-do-list-app/script.js
@@ -1,7 +1,11 @@
-const todo_input=document.getElementById("todo-input");
+document.addEventListener("DOMContentLoaded",()=>{
+    const todo_input=document.getElementById("todo-input");
 const add_task_btn=document.getElementById("add-task-btn");
 const todo_list=document.getElementById("todo-list");
-let tasks=[];
+let tasks=JSON.parse(localStorage.getItem("tasks")) || [];
+tasks.forEach(task => {
+    renderTasks(task);
+});
 add_task_btn.addEventListener("click",()=>{
     let task=todo_input.value.trim();
     if(task==="") return;
@@ -11,6 +15,15 @@ add_task_btn.addEventListener("click",()=>{
         completed:false,
     }
     tasks.push(details);
+    saveTask();
     todo_input.value="";
     console.log(tasks);
+})
+
+function saveTask() {
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+function renderTasks(task) {
+    console.log(task);
+}
 })
